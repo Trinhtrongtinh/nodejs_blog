4fa438657f68559386ba676d9a22591b3682c305
Trinhtrongtinh <trinhtrongtinh1122@gmail.com>
2025-02-20 22:49:17 +0700

    turn2

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,11 @@
+class NewsController {
+    //[GET] /news
+    index(req, res) {
+        res.render('news');
+    }
+    show(req, res) {
+        res.send('NEW DETAILLL!');
+    }
+}
+
+module.exports = new NewsController;
