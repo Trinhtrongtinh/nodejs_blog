f4c2594133f8ce4b38268df098d48bd87399799b
Trinhtrongtinh <trinhtrongtinh1122@gmail.com>
2025-02-20 23:42:41 +0700

    turn3

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,11 +1,11 @@
 class NewsController {
-    //[GET] /news
-    index(req, res) {
-        res.render('news');
-    }
-    show(req, res) {
-        res.send('NEW DETAILLL!');
-    }
+  //[GET] /news
+  index(req, res) {
+    res.render('news');
+  }
+  show(req, res) {
+    res.send('NEW DETAILLL!');
+  }
 }
 
-module.exports = new NewsController;
+module.exports = new NewsController();
--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
-const path = require('path')
+const path = require('path');
 const app = express();
 const port = 3000;
 const route = require('./routes');
-app.use(express.static(path.join(__dirname,'public')));
-app.use(express.urlencoded(
-  {
-    extended:true
-  }
-));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
 app.use(express.json());
-
 
 // Cấu hình Handlebars
 app.engine('hbs', engine({ extname: '.hbs' })); // Định dạng file .hbs
 app.set('view engine', 'hbs');
-app.set('views',path.join(__dirname, 'resources/views')); // Thư mục chứa file template
+app.set('views', path.join(__dirname, 'resources/views')); // Thư mục chứa file template
 //Route init
 route(app);
- 
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
